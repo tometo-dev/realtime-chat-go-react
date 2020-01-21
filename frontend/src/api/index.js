f0f7b4904d7a85b5119999a2a9d3ae0f31400a51
Sudhanshu Ranjan <sudhanshu.ranjan@aptusdatalabs.com>
2020-01-21 15:17:08 +0530

    frontend: opened sample ws connection

---
+++ frontend/src/api/index.js
@@ -0,0 +1,28 @@
+let socket = new WebSocket("ws://localhost:8080/ws")
+
+let connect = () => {
+    console.log("Attempting Connection...")
+
+    socket.onopen = () => {
+        console.log("Successfully Connected")
+    }
+
+    socket.onmessage = msg => {
+        console.log(msg)
+    }
+
+    socket.onclose = event => {
+        console.log("Socket closed connection: ", event)
+    }
+
+    socket.onerror = error => {
+        console.log("Socket error: ", error)
+    }
+}
+
+let sendMsg = msg => {
+    console.log("sending msg: ", msg)
+    socket.send(msg)
+}
+
+export {connect, sendMsg}
