0eb6c83891f24f8538b6190d868f4dde2840950e
Sudhanshu Ranjan <sudhanshu.ranjan@aptusdatalabs.com>
2020-01-21 17:02:04 +0530

    frontend: added chatinput component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import {connect, sendMsg} from "./api"
 import Header from "./components/Header";
 import ChatHistory from "./components/ChatHistory";
+import ChatInput from "./components/ChatInput";
 
 class App extends React.Component {
 
@@ -23,9 +24,11 @@ class App extends React.Component {
         });
     }
 
-    send = () => {
-        console.log("hello")
-        sendMsg("hello")
+    send = (event) => {
+        if (event.keyCode === 13) {
+            sendMsg(event.target.value)
+            event.target.value = ""
+        }
     }
 
     render() {
@@ -33,7 +36,7 @@ class App extends React.Component {
             <div className="App">
                 <Header/>
                 <ChatHistory chatHistory={this.state.chatHistory}/>
-                <button onClick={this.send}>Hit</button>
+                <ChatInput send={this.send}/>
             </div>
         )
     }
